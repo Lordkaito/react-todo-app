6389919dbfe97d5468705ae199b8758387ab9424
Lordkaito <cespedesisai88@gmail.com>
2021-10-28 13:25:05 +0200

    Finish app

---
+++ src/functionBased/components/Navbar.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { MdClose } from 'react-icons/md';
+import { FiMenu } from 'react-icons/fi';
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/about',
+      text: 'About',
+    },
+  ];
+
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
+  return (
+    <nav className='navBar'>
+      <button onClick={handleToggle}>{navbarOpen ? (<MdClose style={{ color: 'red', width: '20px', height: '16px' }} />
+      ) : (
+        <FiMenu style={{ color: '#7b7b7b', width: '20px', height: '16px' }} />
+      )}</button>
+      <ul className={`menuNav ${navbarOpen ? ' showMenu' : ''}`}>
+        {links.map((link) => {
+          return (
+            <li key={link.id}>
+              <NavLink
+                onClick={() => closeMenu()}
+                to={link.path}
+                activeClassName='active-link' exact>
+                {link.text}
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
